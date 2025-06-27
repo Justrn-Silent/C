9919ff2e29a347f26f150f049ec191d8da88920a
Justrn-Silent <139776906+Justrn-Silent@users.noreply.github.com>
2025-06-27 17:53:38 +0800

    创建 While_statement.c

---
+++ C_Code/While_statement.c
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main() {
+	int a = 0;
+	int b = 1;
+	printf("请输入循环次数：\n");
+	scanf("%d",&a);
+	while (a) {
+		printf("第%d次\n",b);
+		a--;
+		b++;
+	}
+	/*
+	语句运行流程：
+	while(条件为真true，或者结果为非零){
+	
+		自增运算;----->>必须要有自增或者自减运算不然有可能程序进入死循环
+
+
+		}
+	程序执行到while先判断条件是否成立，成立即执行否则不执行
+	拓展：前置自增，后置自增/前置自减，后置自减
+	a++:先用后加
+	++a:先加后用
+	详细分析：
+	a++:
+	a=a
+	a=a+1
+	++a:
+	a=a+1
+	a=a
+
+	*/
+return 0;
+}
